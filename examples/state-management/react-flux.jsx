eef3a9765892652b01b5b1dd26ff82e314249f82
Conrad Sollitt <csollitt@hotmail.com>
2020-09-16 13:30:20 -0700

    📄 Minor Code Updates and IE Warnings

--- examples/state-management/react-flux.jsx
+++ examples/state-management/react-flux.jsx
@@ -156,15 +156,15 @@ class CounterLocal extends React.Component {
     }
 
     increment() {
-        this.setState({
-            count: this.state.count + 1,
-        });
+        this.setState(state => ({
+            count: state.count + 1,
+        }));
     }
 
     decrement() {
-        this.setState({
-            count: this.state.count - 1,
-        });
+        this.setState(state => ({
+            count: state.count - 1,
+        }));
     }
 
     render() {
